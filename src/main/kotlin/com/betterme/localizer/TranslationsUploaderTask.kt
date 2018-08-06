d3985a069719fd72160eb4a90f5d50e373ab56af
constmikhailovskiy <konstantin.mikhailovskiy@gen.tech>
2018-08-06 16:38:30 +0300

    Add temporarily hack to avoid Groovy and Kotlin boolean types incompatibility

--- src/main/kotlin/com/betterme/localizer/TranslationsUploaderTask.kt
+++ src/main/kotlin/com/betterme/localizer/TranslationsUploaderTask.kt
@@ -12,17 +12,20 @@ open class TranslationsUploaderTask : DefaultTask() {
     @Input val projectId = project.objects.property(String::class.java)
     @Input val resourcesPath = project.objects.property(String::class.java)
     @Input val locale = project.objects.property(String::class.java)
-    @Input val overwrite = project.objects.property(Boolean::class.java)
+    @Input val overwrite = project.objects.property(String::class.java)
 
     init {
         description = "Uploads terms and translations in a current project for a given locale"
         group = "translations"
     }
 
+    @Suppress("PlatformExtensionReceiverOfInline")
     @TaskAction
     fun uploadTranslations() {
         val apiParams = ApiParams(apiToken = apiToken.get(), projectId = projectId.get())
         val translationsLoader = TranslationsLoaderFactory.create(apiParams)
-        translationsLoader.uploadTermsAndTranslations(resourcesPath.get(), locale.get(), overwrite.get())
+        translationsLoader.uploadTermsAndTranslations(resourcesPath.get(), locale.get(),
+                /* Small temp hack to avoid Groovy and Kotlin boolean incompatibility issue */
+                overwrite.get().toBoolean())
     }
 }
